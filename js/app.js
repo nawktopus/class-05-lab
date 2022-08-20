d4cdcbccb498be5b9d21306e3acbac2d0035d297
Jordan <jkwan104@gmail.com>
2022-08-20 15:57:06 -0700

    added multiply

--- js/app.js
+++ js/app.js
@@ -28,11 +28,13 @@ Write a function called multiply() that takes in two numbers as arguments and re
 Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testMultiply() function and see if the test passes.*/
 
 // Write your code here
-//function multiply(a, b) { //eslint-disable-line
-   
+function multiply(a, b) { //eslint-disable-line
+    let result = a * b;
 
+    return [result,'The product of ' + a + ' and ' + b + ' is ' + result + '.'];  
+}
 // Here is the test for multiply(); uncomment it to run it
- //testMultiply(5,9);
+    testMultiply(5,9);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
