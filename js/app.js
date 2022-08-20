5dd022e818ea112d66c4dcb451aa9638de4698bd
Jordan <jkwan104@gmail.com>
2022-08-20 14:20:55 -0700

    concatenated:

--- js/app.js
+++ js/app.js
@@ -11,7 +11,7 @@ Test this function by hand in the console to get it working, and when you think 
 function sum(a, b) { //eslint-disable-line
     let result = a + b;
 
-    return[result, "The sum of 4 and 7 is 11." ];
+    return[result, 'The sum of ' + a + ' and ' + b + ' is ' + result + '.'];
 }
 
 // Here is the test for sum(); uncomment it to run it
@@ -31,7 +31,7 @@ Test this function by hand in the console to get it working, and when you think 
 function multiply(a, b) { //eslint-disable-line
     let result = a * b;
 
-    return [result,"The product of 5 and 9 is 45."];
+    return [result,'The product of ' + a + ' and ' + b + ' is ' + result + '.'];
 }
 
 // Here is the test for multiply(); uncomment it to run it
