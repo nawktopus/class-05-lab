ec53260345f3bc89dadd120cd1e127884b74965a
Jordan <jkwan104@gmail.com>
2022-08-20 14:12:32 -0700

    Sum Add Multiply

--- js/app.js
+++ js/app.js
@@ -11,7 +11,7 @@ Test this function by hand in the console to get it working, and when you think 
 function sum(a, b) { //eslint-disable-line
     let result = a + b;
 
-    return[result, "The sum of 4 and 7 is 11." ];
+    return[result, 'The sum of ' + a + ' and ' + b + ' is ' + result + '.'];
 }
 
 // Here is the test for sum(); uncomment it to run it
@@ -31,7 +31,7 @@ Test this function by hand in the console to get it working, and when you think 
 function multiply(a, b) { //eslint-disable-line
     let result = a * b;
 
-    return [result,"The product of 5 and 9 is 45."];
+    return [result,'The product of ' + a + ' and ' + b + ' is ' + result + '.'];
 }
 
 // Here is the test for multiply(); uncomment it to run it
@@ -52,11 +52,18 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function sumAndMultiply(a, b, c) { //eslint-disable-line
+    let result = sum(a,b)[0];
+    result = sum(result,c)[0];
+    let result1 = multiply(a,b)[0];
+    result1 = multiply(result1,c)[0];
+
+    return [result,result1,"4 and 7 and 5 sum to 16.","The product of 4 and 7 and 5 is 140."];
 
 }
 
 // Here is the test for sumAndMultiply(); uncomment it to run it
-// testSumAndMultiply(4,7,5);
+
+testSumAndMultiply(4,7,5);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
